ac1510de4c0bcd028d507deac68dd17e3d204033
oleider gonzalez <oleidergonzalez11mx@gmail.com>
2023-06-15 04:36:23 -0700

    added EF and DbContext

---
+++ QuinielaBackend/DbContextQuiniela.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuinielaBackend.Models;
+
+namespace QuinielaBackend
+{
+    public class DbContextQuiniela: DbContext
+    {
+        public DbContextQuiniela(DbContextOptions<DbContextQuiniela> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Games> Games { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Teams> Teams { get; set; }
+        public DbSet<RecordTable> RecordTables { get; set; }
+
+    }
+}
